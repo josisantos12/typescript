16f2bf11f4bc9e604638622fd874f3b06d39e864
josisantos12 <josiane15santos24@gmail.com>
2024-06-24 15:14:52 -0300

    exercicio

---
+++ teste.js
@@ -0,0 +1,11 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplicar(2, 3)); 
+console.log(saudar("João")); 
---
+++ teste.ts
@@ -0,0 +1,11 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+  }
+  
+  // Função de saudação
+  function saudar(nome: string): string {
+    return `Olá ${nome}`;
+  }
+  console.log(multiplicar(2, 3)); 
+console.log(saudar("João")); 
